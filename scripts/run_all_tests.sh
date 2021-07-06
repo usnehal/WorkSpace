290bf57ac3118a0a0f45977203aec85f1be8dd82
Snehal Uphale <snehal.v.uphale@gmail.com>
2021-07-06 23:26:05 +0530

    3rd test improved

--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -1,5 +1,6 @@
 set -e
 
+cd ~/WorkSpace/communication
 python3 ./Client.py -s 35.200.232.85 -t 1 -v 0
 python3 ./Client.py -s 35.200.232.85 -t 2 -v 0
 python3 ./Client.py -s 35.200.232.85 -t 3 -v 0
