cfa9f4991ba9048b4c083f1147c745088469dc7a
Snehal Uphale <snehal.v.uphale@gmail.com>
2021-07-09 15:10:33 +0530

    run scripts

--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -4,18 +4,19 @@ if [ -z "$1" ]
 then
   echo "no server ip"
 else
-  IP=35.200.232.85
+  #IP=35.200.232.85
+  IP=$1
 fi
 
-cd ~/WorkSpace/communication
+cd ~/WorkSpace/communication        
 if [ -z "$IP" ]
 then
-python3 ./Client.py -t 1 -v 0
-python3 ./Client.py -t 2 -v 0
-python3 ./Client.py -t 3 -v 0
+python3 ./Client.py -t 1 -v 0       
+python3 ./Client.py -t 2 -v 0       
+python3 ./Client.py -t 3 -v 0       
 else
-python3 ./Client.py -s 35.200.232.85 -t 1 -v 0
-python3 ./Client.py -s 35.200.232.85 -t 2 -v 0
-python3 ./Client.py -s 35.200.232.85 -t 3 -v 0
+python3 ./Client.py -s $IP -t 1 -v 0
+python3 ./Client.py -s $IP -t 2 -v 0
+python3 ./Client.py -s $IP -t 3 -v 0
 fi
 
