c28bb1e802baf2724b1bfbdc7979aff5671a3d10
Snehal Uphale <snehal.v.uphale@gmail.com>
2021-07-15 10:06:07 +0530

    imagesize tests added

--- inference.py
+++ inference.py
@@ -8,23 +8,25 @@
 import  time
 import  pandas as pd
 import  numpy as np
-from    skimage import io
-import  random
-from    collections import Counter
-from    tensorflow.keras.preprocessing.text import Tokenizer
+# from    skimage import io
+# import  random
+# from    collections import Counter
+# from    tensorflow.keras.preprocessing.text import Tokenizer
 import  tensorflow as tf
-from    tensorflow import keras
-from    tensorflow.keras import layers,Model
+# from    tensorflow import keras
+# from    tensorflow.keras import layers,Model
 from    tqdm import tqdm
-from    nltk.translate.bleu_score import sentence_bleu
-import  socket
-import  pickle5 as pickle
-from    tensorflow.keras.activations import tanh
-from    tensorflow.keras.activations import softmax
-import  matplotlib.pyplot as plt
+# from    nltk.translate.bleu_score import sentence_bleu
+# import  socket
+# import  pickle5 as pickle
+# from    tensorflow.keras.activations import tanh
+# from    tensorflow.keras.activations import softmax
+# import  matplotlib.pyplot as plt
 import  time
 import  argparse
 from    sklearn.metrics import accuracy_score
+import  tensorflow_datasets as tfds
+import  functools
 
 
 # In[2]:
@@ -41,12 +43,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-s', '--server', action='store', type=str, required=False)
 parser.add_argument('-t', '--test_number', action='store', type=int, required=False)
 parser.add_argument('-v', '--verbose', action='store', type=int, required=False)
+parser.add_argument('-i', '--imagesize', action='store', type=int, required=False)
 args, unknown = parser.parse_known_args()
 print(args.server)
 
 server_ip = args.server
 test_number = args.test_number
 verbose = args.verbose
+imagesize = args.imagesize
 
 if(verbose == None):
     verbose = 1
@@ -58,18 +62,11 @@ test_scenarios = {  1:"Complete jpg file buffer transfer",
                     2:"Decoded image buffer transfer",
                     3:"Decoded image buffer transfer with zlib compression"}
 
-print("Test scenario = %d %s" % (test_number, test_scenarios[test_number]))
+if(imagesize == None):
+    imagesize = 299
 
 
-# In[4]:
-
-
-total_test_images = 100
-batch_size = 32
-PREDICTIONS_THRESHOLD = 0.4
-
-
-# In[5]:
+# In[ ]:
 
 
 Logger.set_log_level(verbose)
@@ -79,14 +76,27 @@ client = Client(cfg)
 imagesInfo = ImagesInfo(cfg)
 
 
+# In[4]:
+
+
+total_test_images = 100
+batch_size = 32
+PREDICTIONS_THRESHOLD = 0.4
+h_image_height = imagesize
+h_image_width = imagesize
+
+Logger.milestone_print("Test scenario = %d %s" % (test_number, test_scenarios[test_number]))
+Logger.milestone_print("Image shape = (%d %d)" % (h_image_height, h_image_width))
+
+
 # In[6]:
 
 
 # new_head_model = tf.keras.models.load_model(cfg.temp_path + '/new_head_model')
 # new_tail_model = tf.keras.models.load_model(cfg.temp_path + '/new_tail_model')
 
-new_head_model = tf.keras.models.load_model(cfg.saved_model_path + '/new_head_model')
-new_tail_model = tf.keras.models.load_model(cfg.saved_model_path + '/new_tail_model')
+new_head_model = tf.keras.models.load_model(cfg.saved_model_path + '/head_model')
+new_tail_model = tf.keras.models.load_model(cfg.saved_model_path + '/tail_model')
 
 
 # In[7]:
@@ -132,7 +142,8 @@ def process_predictions(ground_truth, prediction_tensor):
                 top_5_accuracy = 1.0
 
         precision = TP / predictions_length
-    recall = TP / ground_truth_length
+    if(predictions_length > 0):
+        recall = TP / ground_truth_length
     return accuracy, top_1_accuracy,top_5_accuracy,precision,recall, top_predictions, predictions_str
 
 
@@ -146,29 +157,105 @@ def evaluate_classification(image):
     return s
 
 
-# In[9]:
+# In[10]:
 
 
-total_time = 0.0
-max_test_images = total_test_images
+data_dir='/home/suphale/coco'
+N_LABELS = 80
+split_val = "validation[:20%]"
+# h_image_height = 299
+# h_image_width = 299
+
+
+# In[11]:
+
+
+class BoxField:
+    BOXES = 'bbox'
+    KEYPOINTS = 'keypoints'
+    LABELS = 'label'
+    MASKS = 'masks'
+    NUM_BOXES = 'num_boxes'
+    SCORES = 'scores'
+    WEIGHTS = 'weights'
+
+class DatasetField:
+    IMAGES = 'images'
+    IMAGES_INFO = 'images_information'
+    IMAGES_PMASK = 'images_padding_mask'
+
+def my_preprocess(inputs):
+    image = inputs['image']
+    image = tf.image.resize(image, (h_image_height, h_image_width))
+    image = tf.cast(image, tf.float32)
+    image /= 127.5
+    image -= 1.
+
+    targets = inputs['objects']
+
+    image_information = tf.cast(tf.shape(image)[:2], dtype=tf.float32)
+
+    inputs = {DatasetField.IMAGES: image, DatasetField.IMAGES_INFO: image_information}
+
+    # ground_truths = {
+    #     BoxField.BOXES: targets[BoxField.BOXES] * tf.tile(image_information[tf.newaxis], [1, 2]),
+    #     BoxField.LABELS: tf.cast(targets[BoxField.LABELS], tf.int32),
+    #     BoxField.NUM_BOXES: tf.shape(targets[BoxField.LABELS]),
+    #     BoxField.WEIGHTS: tf.fill(tf.shape(targets[BoxField.LABELS]), 1.0)
+    # }
+    ground_truths = tf.cast(targets[BoxField.LABELS], tf.int32)
+    # ground_truths = tf.one_hot(ground_truths, depth=N_LABELS, dtype=tf.int32)
+    # ground_truths = tf.reduce_sum(ground_truths, 0)
+    # ground_truths = tf.greater( ground_truths, tf.constant( 0 ) )    
+    # ground_truths = tf.where (ground_truths, 1, 0) 
+    return image, ground_truths
+
+def expand_dims_for_single_batch(image, ground_truths):
+    image = tf.expand_dims(image, axis=0)
+    ground_truths = tf.expand_dims(ground_truths, axis=0)
+    return image, ground_truths
+
+
+# In[12]:
+
+
+ds_val = tfds.load(name="coco/2017", split=split_val, data_dir=data_dir, shuffle_files=False, download=False)
+ds_val = ds_val.map(functools.partial(my_preprocess), num_parallel_calls=tf.data.experimental.AUTOTUNE)
+ds_val = ds_val.map(expand_dims_for_single_batch, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+# ds_val = ds_val.range(100)
+
+# iterator = ds_val.make_one_shot_iterator()
+
+ds_val = ds_val.prefetch(tf.data.experimental.AUTOTUNE)
+
+
+# In[13]:
+
+
 df = pd.DataFrame(columns=['img_path','ground_truth', 'top_predict', 'Prediction', 'accuracy', 'top_1_accuracy', 'top_5_accuracy', 'precision', 'recall', 'time'])
 
+total_time = 0.0
+# for test_index in range(10):
+count = 0
+max_test_images = 5000
+# for sample_img_batch, ground_truth in ds_val:
 for i in tqdm(range(max_test_images)):
-    Logger.debug_print("")
-    img_path = imagesInfo.all_img_vector[i]
-    # plt.imshow(image)
-    ground_truth = imagesInfo.get_segmentation_id_indexes(img_path)
-
-    test_image = img_path
-
+# for sample_img_batch, ground_truth in ds_val:
+    count += 1
+    sample_img_batch, ground_truth = next(iter(ds_val))
+    tensor_shape = len(ground_truth.get_shape().as_list())
+    if(tensor_shape > 1):
+        ground_truth = tf.squeeze(ground_truth,[0])
+    # print(ground_truth)
     t0= time.perf_counter()
-    s = evaluate_classification(test_image)
+    h = new_head_model(sample_img_batch)
+    s = new_tail_model(h)
     t1 = time.perf_counter() - t0
     total_time = total_time + t1
 
     accuracy, top_1_accuracy,top_5_accuracy,precision,recall, top_predictions, predictions_str = process_predictions(ground_truth,s)
     df = df.append(
-        {'image':img_path, 
+        {'image':i, 
         'ground_truth':(str(imagesInfo.get_segmentation_texts(ground_truth))),
         'top_predict':str(top_predictions),
         'Prediction':predictions_str,
@@ -187,18 +274,14 @@ for i in tqdm(range(max_test_images)):
 df.to_csv(cfg.temp_path + '/results_'+cfg.timestr+'.csv')
 av_column = df.mean(axis=0)
 
+print(count)
+
 Logger.milestone_print("accuracy        : %.2f" % (av_column.accuracy))
 Logger.milestone_print("top_1_accuracy  : %.2f" % (av_column.top_1_accuracy))
 Logger.milestone_print("top_5_accuracy  : %.2f" % (av_column.top_5_accuracy))
 Logger.milestone_print("precision       : %.2f" % (av_column.precision))
 Logger.milestone_print("recall          : %.2f" % (av_column.recall))
 Logger.milestone_print("time            : %.2f" % (av_column.time))
-
-
-# In[10]:
-
-
-
 
 
 # In[ ]:
