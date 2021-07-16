9c13ed0beb56e6bc2c593a540bcec8f6fc8881b2
Snehal Uphale <snehal.v.uphale@gmail.com>
2021-07-16 10:20:09 +0530

    cleanup

--- scripts/exp_01.sh
+++ scripts/exp_01.sh
@@ -2,23 +2,23 @@ set -e
 
 
 cd ~/WorkSpace/       
-python3 ./inference.py -i 25
-python3 ./inference.py -i 50
-python3 ./inference.py -i 75
-python3 ./inference.py -i 100
-python3 ./inference.py -i 125
-python3 ./inference.py -i 150
-python3 ./inference.py -i 175
-python3 ./inference.py -i 200
-python3 ./inference.py -i 225
-python3 ./inference.py -i 250
-python3 ./inference.py -i 275
-python3 ./inference.py -i 300
-python3 ./inference.py -i 325
-python3 ./inference.py -i 350
-python3 ./inference.py -i 375
-python3 ./inference.py -i 400
-python3 ./inference.py -i 425
-python3 ./inference.py -i 450
-python3 ./inference.py -i 475
-python3 ./inference.py -i 500
+python3 ./client.py -t 0 -v 0 -m 1000 -i 25
+python3 ./client.py -t 0 -v 0 -m 1000 -i 50
+python3 ./client.py -t 0 -v 0 -m 1000 -i 75
+python3 ./client.py -t 0 -v 0 -m 1000 -i 100
+python3 ./client.py -t 0 -v 0 -m 1000 -i 125
+python3 ./client.py -t 0 -v 0 -m 1000 -i 150
+python3 ./client.py -t 0 -v 0 -m 1000 -i 175
+python3 ./client.py -t 0 -v 0 -m 1000 -i 200
+python3 ./client.py -t 0 -v 0 -m 1000 -i 225
+python3 ./client.py -t 0 -v 0 -m 1000 -i 250
+python3 ./client.py -t 0 -v 0 -m 1000 -i 275
+python3 ./client.py -t 0 -v 0 -m 1000 -i 300
+python3 ./client.py -t 0 -v 0 -m 1000 -i 325
+python3 ./client.py -t 0 -v 0 -m 1000 -i 350
+python3 ./client.py -t 0 -v 0 -m 1000 -i 375
+python3 ./client.py -t 0 -v 0 -m 1000 -i 400
+python3 ./client.py -t 0 -v 0 -m 1000 -i 425
+python3 ./client.py -t 0 -v 0 -m 1000 -i 450
+python3 ./client.py -t 0 -v 0 -m 1000 -i 475
+python3 ./client.py -t 0 -v 0 -m 1000 -i 500
