b82b5703079f8b6c692d97906ffedf9926ef6053
Snehal Uphale <snehal.v.uphale@gmail.com>
2021-07-25 16:05:07 +0530

    all tests expanded

--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -17,10 +17,23 @@ VERBOSE=" -v 0"
 cd ~/WorkSpace/
 rm ./temp/results/results.csv 
 python3 ./client.py $SERVER -t 1 $VERBOSE $TESTS --image_size 100
+python3 ./client.py $SERVER -t 1 $VERBOSE $TESTS --image_size 150
+python3 ./client.py $SERVER -t 1 $VERBOSE $TESTS --image_size 200
 python3 ./client.py $SERVER -t 1 $VERBOSE $TESTS --image_size 250
+python3 ./client.py $SERVER -t 1 $VERBOSE $TESTS --image_size 300
+python3 ./client.py $SERVER -t 1 $VERBOSE $TESTS --image_size 350
+python3 ./client.py $SERVER -t 1 $VERBOSE $TESTS --image_size 400
 python3 ./client.py $SERVER -t 2 $VERBOSE $TESTS
 python3 ./client.py $SERVER -t 3 $VERBOSE $TESTS
 python3 ./client.py $SERVER -t 4 $VERBOSE $TESTS
-python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --split_layer 40
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --split_layer 3
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --split_layer 40
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --split_layer 63
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --split_layer 86
 python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --split_layer 100
-
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --split_layer 132
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --split_layer 164
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --split_layer 196
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --split_layer 228
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --split_layer 248
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --split_layer 279
