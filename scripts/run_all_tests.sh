038869497ccc744c078060eb23133dfe3d8f6f45
Snehal Uphale <snehal.v.uphale@gmail.com>
2021-08-01 06:51:15 +0530

    expanded tests

--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -23,30 +23,176 @@ python3 ./client.py $SERVER -t 1 $VERBOSE $TESTS --image_size 250
 python3 ./client.py $SERVER -t 1 $VERBOSE $TESTS --image_size 300
 python3 ./client.py $SERVER -t 1 $VERBOSE $TESTS --image_size 350
 python3 ./client.py $SERVER -t 1 $VERBOSE $TESTS --image_size 400
+
 python3 ./client.py $SERVER -t 2 $VERBOSE $TESTS
 python3 ./client.py $SERVER -t 3 $VERBOSE $TESTS
 python3 ./client.py $SERVER -t 4 $VERBOSE $TESTS
 
-python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --split_layer 3
-python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --split_layer 40
-python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --split_layer 63
-python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --split_layer 86
-python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --split_layer 100
-python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --split_layer 132
-python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --split_layer 164
-python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --split_layer 196
-python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --split_layer 228
-python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --split_layer 248
-python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --split_layer 279
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 100 --split_layer 3
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 100 --split_layer 40
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 100 --split_layer 63
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 100 --split_layer 86
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 100 --split_layer 100
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 100 --split_layer 132
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 100 --split_layer 164
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 100 --split_layer 196
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 100 --split_layer 228
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 100 --split_layer 248
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 100 --split_layer 279
 
-python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --split_layer 3
-python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --split_layer 40
-python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --split_layer 63
-python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --split_layer 86
-python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --split_layer 100
-python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --split_layer 132
-python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --split_layer 164
-python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --split_layer 196
-python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --split_layer 228
-python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --split_layer 248
-python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --split_layer 279
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 150 --split_layer 3
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 150 --split_layer 40
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 150 --split_layer 63
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 150 --split_layer 86
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 150 --split_layer 100
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 150 --split_layer 132
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 150 --split_layer 164
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 150 --split_layer 196
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 150 --split_layer 228
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 150 --split_layer 248
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 150 --split_layer 279
+
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 200 --split_layer 3
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 200 --split_layer 40
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 200 --split_layer 63
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 200 --split_layer 86
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 200 --split_layer 100
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 200 --split_layer 132
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 200 --split_layer 164
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 200 --split_layer 196
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 200 --split_layer 228
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 200 --split_layer 248
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 200 --split_layer 279
+
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 250 --split_layer 3
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 250 --split_layer 40
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 250 --split_layer 63
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 250 --split_layer 86
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 250 --split_layer 100
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 250 --split_layer 132
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 250 --split_layer 164
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 250 --split_layer 196
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 250 --split_layer 228
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 250 --split_layer 248
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 250 --split_layer 279
+
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 300 --split_layer 3
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 300 --split_layer 40
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 300 --split_layer 63
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 300 --split_layer 86
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 300 --split_layer 100
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 300 --split_layer 132
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 300 --split_layer 164
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 300 --split_layer 196
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 300 --split_layer 228
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 300 --split_layer 248
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 300 --split_layer 279
+
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 350 --split_layer 3
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 350 --split_layer 40
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 350 --split_layer 63
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 350 --split_layer 86
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 350 --split_layer 100
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 350 --split_layer 132
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 350 --split_layer 164
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 350 --split_layer 196
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 350 --split_layer 228
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 350 --split_layer 248
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 350 --split_layer 279
+
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 400 --split_layer 3
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 400 --split_layer 40
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 400 --split_layer 63
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 400 --split_layer 86
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 400 --split_layer 100
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 400 --split_layer 132
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 400 --split_layer 164
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 400 --split_layer 196
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 400 --split_layer 228
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 400 --split_layer 248
+python3 ./client.py $SERVER -t 5 $VERBOSE $TESTS --image_size 400 --split_layer 279
+
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 100 --split_layer 3
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 100 --split_layer 40
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 100 --split_layer 63
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 100 --split_layer 86
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 100 --split_layer 100
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 100 --split_layer 132
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 100 --split_layer 164
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 100 --split_layer 196
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 100 --split_layer 228
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 100 --split_layer 248
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 100 --split_layer 279
+
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 150 --split_layer 3
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 150 --split_layer 40
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 150 --split_layer 63
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 150 --split_layer 86
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 150 --split_layer 100
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 150 --split_layer 132
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 150 --split_layer 164
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 150 --split_layer 196
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 150 --split_layer 228
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 150 --split_layer 248
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 150 --split_layer 279
+
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 200 --split_layer 3
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 200 --split_layer 40
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 200 --split_layer 63
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 200 --split_layer 86
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 200 --split_layer 100
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 200 --split_layer 132
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 200 --split_layer 164
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 200 --split_layer 196
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 200 --split_layer 228
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 200 --split_layer 248
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 200 --split_layer 279
+
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 250 --split_layer 3
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 250 --split_layer 40
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 250 --split_layer 63
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 250 --split_layer 86
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 250 --split_layer 100
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 250 --split_layer 132
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 250 --split_layer 164
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 250 --split_layer 196
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 250 --split_layer 228
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 250 --split_layer 248
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 250 --split_layer 279
+
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 300 --split_layer 3
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 300 --split_layer 40
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 300 --split_layer 63
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 300 --split_layer 86
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 300 --split_layer 100
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 300 --split_layer 132
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 300 --split_layer 164
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 300 --split_layer 196
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 300 --split_layer 228
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 300 --split_layer 248
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 300 --split_layer 279
+
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 350 --split_layer 3
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 350 --split_layer 40
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 350 --split_layer 63
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 350 --split_layer 86
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 350 --split_layer 100
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 350 --split_layer 132
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 350 --split_layer 164
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 350 --split_layer 196
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 350 --split_layer 228
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 350 --split_layer 248
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 350 --split_layer 279
+
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 400 --split_layer 3
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 400 --split_layer 40
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 400 --split_layer 63
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 400 --split_layer 86
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 400 --split_layer 100
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 400 --split_layer 132
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 400 --split_layer 164
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 400 --split_layer 196
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 400 --split_layer 228
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 400 --split_layer 248
+python3 ./client.py $SERVER -t 6 $VERBOSE $TESTS --image_size 400 --split_layer 279
+
